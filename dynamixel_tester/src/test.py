655d0a53601d7819f19822290bbb55ad943d9ce1
fontyssmartwrist <52158519+fontyssmartwrist@users.noreply.github.com>
2019-07-08 16:05:01 +0200

    Update test.py

--- dynamixel_tester/src/test.py
+++ dynamixel_tester/src/test.py
@@ -1,24 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from dynamixel_workbench_msgs.srv import DynamixelCommand
-
-def dynamixel_tester():
-    pub = rospy.Publisher('chatter', JointTrajectory, queue_size=10)
-    rospy.init_node('dynamixel_tester', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    
-    trajectory = JointTrajectory()
-
-    trajectory.joint_names = ['End_link']
-    trajectory.points = JointTrajectoryPoint()
-
-    jtp = JointTrajectoryPoint()
-
-    jtp.positions = [3.14]
-    trajectory.points.append(jtp)
-    pub.publish(trajectory)
 
 def call_dymanixel_service(setpoint):
     rospy.wait_for_service('dynamixel_workbench/dynamixel_command')
@@ -32,7 +15,6 @@ def call_dymanixel_service(setpoint):
 
 if __name__ == '__main__':
     try:
- #       dynamixel_tester()
         call_dymanixel_service(0)
         rospy.sleep(1)
         call_dymanixel_service(4095)
